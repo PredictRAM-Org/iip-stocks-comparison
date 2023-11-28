f5b1b778798ab1846f7ba95fa0390067517e36bb
PredictRAM DataHub <95133921+PredictRAM-Org@users.noreply.github.com>
2023-11-29 01:11:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 
 # Function to load IIP data
 def load_iip_data():
-    iip_file_path = 'IP_Data.xlsx'  # Replace with the actual path
+    iip_file_path = 'IIP_Data.xlsx'  # Replace with the actual path
     iip_data = pd.read_excel(iip_file_path)
     return iip_data
 
